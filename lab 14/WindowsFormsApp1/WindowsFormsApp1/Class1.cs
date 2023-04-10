05c0f264e7ed7887b741386c6252bd146977c5af
GeorgeKuydin <125215800+GeorgeKuydin@users.noreply.github.com>
2023-04-10 22:48:44 +0300

    Add files via upload

---
+++ lab 14/WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class SaveAsThread
+    {
+        private int[] array;
+        private string fileName;
+        private ProgressBar progressBar;
+
+        public SaveAsThread(int[] array, string fileName, ProgressBar progressBar)
+        {
+            this.array = array;
+            this.fileName = fileName;
+            this.progressBar = progressBar;
+        }
+
+        public void SaveArrayToFile()
+        {
+            Thread saveThread = new Thread(() =>
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        writer.WriteLine(array[i]);
+
+                        progressBar.Invoke((MethodInvoker)delegate
+                        {
+                            progressBar.Value = (int)(((double)i / array.Length) * 100);
+                        });
+                    }
+                }
+            });
+
+            saveThread.Start();
+        }
+    }
+}
---
+++ lab 14/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = { 4, 5, 6 };
+            int[] array3 = { 7, 8, 9 };
+
+            SaveAsThread thread1 = new SaveAsThread(array1, "array1.txt", progressBar1);
+            SaveAsThread thread2 = new SaveAsThread(array2, "array2.txt", progressBar2);
+            SaveAsThread thread3 = new SaveAsThread(array3, "array3.txt", progressBar3);
+
+            Thread t1 = new Thread(new ThreadStart(thread1.SaveArrayToFile));
+            Thread t2 = new Thread(new ThreadStart(thread2.SaveArrayToFile));
+            Thread t3 = new Thread(new ThreadStart(thread3.SaveArrayToFile));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
